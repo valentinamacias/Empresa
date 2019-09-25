e8913ef1128e8950ac3b824a0b2cbd44dda62a6c
valentinamacias <51164860+valentinamacias@users.noreply.github.com>
2019-09-25 12:55:29 -0500

    version  1.0
    crear atributos de clase

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,26 @@
+package empresa;
+
+import java.util.Date;
+
+public class Empresa { 
+        
+     private int numeroDeEmpleados;
+     private Date fechaDeFundacion;
+     private String nombreDeLaEmpresa;
+     private boolean sueldo;
+             
+}
+
+
+
+            
+             
+             
+    
+    
+    
+
+   
+
+    
+   
